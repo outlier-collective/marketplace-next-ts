f80760074c63940a3f21f5919375cf2bc7c1a223
Matthew  H <matthewh@Matthews-MacBook-Pro.local>
2022-07-26 19:08:26 -0700

    progress

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,5 +1,22 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
+import { useNFTDrop, useNFTs, ThirdwebNftMedia } from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+// TASKS 
+/* 
+    Mint Starter Kit
+    View Starter Kits
+    Buy Booster Kit
+    View Booster Kits
+    Choose a starter Kit
+    Choose a booster kit
+    Choose an item from booster kit
+    Submit selections
+    Show new fresh starter Kit
+    Use Starter Kit to Summon Val
+*/
+
 
 // ROLL
 /*
@@ -24,14 +41,44 @@ if (error) {
 }
 */
 
+/*
+const PROVIDER_URL = process.env.PROVIDER_URL_RINKEBY;
+const NFT_COLLECTION_ADDRESS = process.env.NFT_COLLECTION_ADDRESS_RINKEBY;
+*/
+
 
 const Mint: NextPage = () => {
-  
+
+
+    // Use Ethers to get contract
+    // 
+
+    
+
+    /*
+    const nftContract = new ethers.Contract(
+        NFT_COLLECTION_ADDRESS,
+        marketplaceContractAbi,
+        provider
+    );*/
+
+
     return (
+
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2">
             <a className={styles.mainButton}>
                 Mint
             </a>
+
+            <div>
+                {nfts?.map((nft, id) => (
+                    <>
+                    <ThirdwebNftMedia key={id} metadata={nft.metadata} />
+                    <h3>{nft.metadata.name}</h3>
+                    </>
+                ))}
+            </div>
+            
         </div>
     )
 }
